78c6e663d47c586881fd774264408f86fc97a119
Martijn Koster <martijn.koster.94@gmail.com>
2023-05-25 21:32:56 +0200

    park accessibility updated
    takes 5 minutes to run for 1000 points

--- R/accessibility_fake_entrance.R
+++ R/accessibility_fake_entrance.R
@@ -2,16 +2,20 @@
 #'
 #' @param address A spatial object representing the location of interest, the location should be in projected coordinates.
 #' @param buffer_distance A distance in meters to create a buffer or isochrone around the address location
-#' @param net An optional \code{sfnetworks} object representing a road network
 #' @param parks A spatial object representing parks
 #' @param UID A character string representing a unique identifier for each point of interest
+#' @param network_file An optional sfnetwork object representing a road network, If missing the road network will be created.
+#' @param speed A numeric value representing the speed in km/h to calculate the buffer distance (required if `time` is provided)
+#' @param time A numeric value representing the travel time in minutes to calculate the buffer distance (required if `speed` is provided)
+#' @param city When using a network buffer, you can add a city where your address points are to speed up the process
 #'
 #' @return The distance to the nearest fake entries of parks and whether the parks are within the buffer distance that is given
 #' @export
 #'
 #' @examples
-parks_access_entrance <- function(address, buffer_distance = 300, net, parks, UID) {
-
+parks_access_entrance <- function(address, parks = NULL, buffer_distance = NULL, network_file=NULL,
+                                  speed=NULL, time=NULL, city=NULL, UID=NULL) {
+  # timer for function
   start_function <- Sys.time()
   ### Make sure main data set has projected CRS and save it
 
@@ -22,97 +26,78 @@ parks_access_entrance <- function(address, buffer_distance = 300, net, parks, UI
   }
   projected_crs <- sf::st_crs(address_location)
 
-  ### Creating net if not given
-  if (missing(net)){
-    ### Address into point
+  if (missing(network_file)){
+    message('You did not provide a network file, a network will be created using osm.')
+    if (missing(buffer_distance)){
+      if(missing(speed)||missing(time)){
+        stop("You didn't enter speed and time or the buffer distance,
+             please enter speed and time, or the buffer distance.")
+      } else if (!speed > 0) {
+        stop("Speed must be a positive integer")
+      } else if (!time > 0) {
+        stop("Time must be a positive integer")
+      } else{
+        buffer_distance <- speed * 1000/ 60 * time
+      }
+    }
     if ("POINT" %in% sf::st_geometry_type(address_location)) {
-      #Do nothing
     }else if (missing(buffer_distance)) {
-      stop("You do not have a point geometry and did not provide a buffer, please provide a point geometry or a buffer distance")
+      stop("You do not have a point geometry and did not provide a buffer,
+      or speed and time,
+           please provide a point geometry or a buffer distance (or spede and time)")
     }
     else {
       message('There are nonpoint geometries, they will be converted into centroids')
-      address_location <- sf::st_centroid(address_location)}
-    start <- Sys.time()
-
+      address_location <- sf::st_centroid(address_location)
+    }
     iso_area <- sf::st_buffer(sf::st_convex_hull(
       sf::st_union(sf::st_geometry(address_location))),
       buffer_distance)
     iso_area <- sf::st_transform(iso_area, crs = 4326)
+    # bbox might be redundant
     bbox <- sf::st_bbox(iso_area)
-    q <- osmdata::opq(bbox) %>%
-      osmdata::add_osm_feature(key = "highway") %>%
-      osmdata::osmdata_sf()
-    print(Sys.time()-start)
-    print('Time to calculate the osmdata sf')
+    # Use the osmextract package to extract the lines in the area.
+    if (!missing(city)) {
+      warning('If a city is missing, it will take more time to run the function')
+      start<-Sys.time()
+      lines <- osmextract::oe_get(city, stringsAsFactors=FALSE, boundary=iso_area, max_file_size = 5e+09, boundary_type = 'spat')
+      print(Sys.time()-start)
 
-    start <- Sys.time()
+    } else{
+      start<-Sys.time()
+      lines <- osmextract::oe_get(iso_area, stringsAsFactors=FALSE, boundary=iso_area, max_file_size = 5e+09, boundary_type = 'spat')
+      print(Sys.time()-start)
+    }
 
-    # extract lines and polygons from the OSM data
-    lines <- q$osm_lines
-    polys <- q$osm_polygons
+    ## We might need the multilinestrings as well?
+    # start<-Sys.time()
+    # b <- osmextract::oe_get(iso_area, stringsAsFactors=FALSE,quiet=T, layer='multilinestrings', boundary=iso_area, boundary_type = 'spat')
+    # print(Sys.time()-start)
 
-
-    #Remove Factors
-    lines$osm_id <- as.numeric(as.character(lines$osm_id))
-    polys$osm_id <- as.numeric(as.character(polys$osm_id))
-
-    #remove the invalid polygons
-    polys <- polys[!is.na(polys$highway),]
-    polys <- polys[is.na(polys$area),]
-
-    #Change Polygons to Lines
-    polys <- sf::st_cast (polys, "LINESTRING")
-    lines <- sf::st_cast (lines, "LINESTRING")
-
-    # remove invalid geometry
-    #lines <- lines[st_is_valid(lines) %in% TRUE,] # %in% TRUE handles NA that occure with empty geometries
-    polys <- polys[sf::st_is_valid(polys) %in% TRUE,]
-    #Bind together
-    lines <- rbind(lines,polys)
-
-    print(Sys.time()-start)
-    print('time to bind lines and polys together from osm data sf')
-
-
-    #Change to user projection
-    start <- Sys.time()
+    # now I have the lines I want.
     lines <- sf::st_transform(lines, projected_crs)
     lines <- tidygraph::select(lines, "osm_id", "name")
+
     region_shp <- sf::st_transform(iso_area, projected_crs)
-    print(Sys.time()-start)
-    print('time to change user projection')
 
     #Download osm used a square bounding box, now trim to the exact boundary
     #note that lines that that cross the boundary are still included
-
-    start <- Sys.time()
     lines <- lines[region_shp,]
-    print(Sys.time()-start)
-    print('time to trim the exact boundary')
 
+    # Round coordinates to 0 digits.
     sf::st_geometry(lines) <- sf::st_geometry(lines) %>%
       sf::st_sfc(crs = sf::st_crs(lines))
 
-
-
-    start <- Sys.time()
     # Network
-    net <- sfnetworks::as_sfnetwork(lines, directed = FALSE)
-    net <- tidygraph::convert(net, sfnetworks::to_spatial_subdivision)
-    print(Sys.time()-start)
-    print('time to make the network and convert it into spatial subdivision')
+    network_file <- sfnetworks::as_sfnetwork(lines, directed = FALSE)
+    network_file <- tidygraph::convert(network_file, sfnetworks::to_spatial_subdivision)
 
-
-
-    start <-Sys.time()
     #convert network to an sf object of edge
-    net_sf <- net %>% tidygraph::activate("edges") %>%
+    start=Sys.time()
+    net_sf <- network_file %>% tidygraph::activate("edges") %>%
       sf::st_as_sf()
     print(Sys.time()-start)
-    print('time to activate edges')
-
-    start <- Sys.time()
+    print('To activate edges')
     # Find which edges are touching each other
     touching_list <- sf::st_touches(net_sf)
     # create a graph from the touching list
@@ -128,38 +113,26 @@ parks_access_entrance <- function(address, buffer_distance = 300, net, parks, UI
 
     # Subset the edges corresponding to the biggest connected component
     osm_connected_edges <- net_sf[roads_group$membership == biggest_group, ]
-
-    print(Sys.time()-start)
-    print('Time to subset the edges corrsponding to the biggest connected component')
     # Filter nodes that are not connected to the biggest connected component
-    start <- Sys.time()
-    net <- net %>%
+    network_file <- network_file %>%
       tidygraph::activate("nodes") %>%
       sf::st_filter(osm_connected_edges, .pred = sf::st_intersects)
-    print(Sys.time()-start)
-    print('Time to activate nodes that are connected to biggest compontnt')
 
-
-    net_intersect <- sf::st_transform(net,  crs = 4326)
+    net_intersect <- sf::st_transform(network_file,  crs = 4326)
     net_points <- sf::st_geometry(net_intersect)
   }
+
   else  {
     # If the CRS of the network data set is geographic, tranfrom it to the project CRS
-    if (sf::st_crs(address_location) != sf::st_crs(net))
+    if (sf::st_crs(address_location) != sf::st_crs(nework_file))
     {
       print("The CRS of your network data set is geographic, CRS of main data set will be used to transform")
-      net <- sf::st_transform(net, projected_crs)
+      network_file <- sf::st_transform(network_file, projected_crs)
     }
   }
-
-
-  ### Adding edge length for future calculations
-  start <- Sys.time()
-  net <- tidygraph::mutate(tidygraph::activate(net, "edges"), weight = sfnetworks::edge_length())
-  print(Sys.time()-start)
-  print('time to add edge length for future calc')
-
-  ### Creating parks set if not given
+  # Compute the edge weights bsased on their length
+  network_file <- tidygraph::mutate(tidygraph::activate(network_file, "edges"),
+                                    weight = sfnetworks::edge_length())
 
   if (missing(parks)) {
     start <- Sys.time()
@@ -193,62 +166,80 @@ parks_access_entrance <- function(address, buffer_distance = 300, net, parks, UI
     parks_buffer <- sf::st_buffer(parks, 20)
     print('Clean parks')
     print(Sys.time()-start)
+    st_intersection_faster <- function(x,y){
+      #faster replacement for st_intersection(x, y,...)
 
-    fake_entrance <- sf::st_intersection(net_points, parks_buffer)
+      y_subset <-
+        sf::st_intersects(x, y) %>%
+        unlist() %>%
+        unique() %>%
+        sort() %>%
+        {y[.,]}
+
+      sf::st_intersection(x, y_subset)
+    }
+    fake_entrance <- st_intersection_faster(net_points, parks_buffer)
     fake_entrance <- fake_entrance %>% sf::st_transform(crs=projected_crs)
     print('time to calculate fake entrances parks')
     print(Sys.time()-start)
 
   }
-    else {
-      if (sf::st_crs(address_location) != sf::st_crs(parks))
-      {
-        message("The CRS of your parks data set is geographic, CRS of main data set will be used to transform")
-        parks <- sf::st_transform(parks, original_pr)
-      }
-
-      if ("POINT" %in% sf::st_geometry_type(parks)){
-        # Do nothing
-      } else {
-        message('Fake entrances for the given park polygons will be created')
-        parks_buffer <- sf::st_buffer(parks, 20)
-        fake_entrance <- sf::st_intersection(net_points, parks_buffer)
-        fake_entrance <- fake_entrance %>% sf::st_transform(crs=projected_crs)
-      }
+  else {
+    if (sf::st_crs(address_location) != sf::st_crs(parks))
+    {
+      message("The CRS of your parks data set is geographic, CRS of main data set will be used to transform")
+      parks <- sf::st_transform(parks, projected_crs)
     }
 
-    ### Calculations
+    if ("POINT" %in% sf::st_geometry_type(parks)){
+      # Do nothing
+    } else {
+      message('Fake entrances for the given park polygons will be created')
+      parks_buffer <- sf::st_buffer(parks, 20)
+      fake_entrance <- sf::st_intersection(net_points, parks_buffer)
+      fake_entrance <- fake_entrance %>% sf::st_transform(crs=projected_crs)
+    }
+  }
+
+  ### Calculations
   start <- Sys.time()
-    add_park_dist <- as.data.frame(sfnetworks::st_network_cost(net, from = address_location, to = fake_entrance))
-    closest_park <- apply(add_park_dist, 1, FUN = min)
-    parks_in_buffer <- ifelse((rowSums(units::drop_units(add_park_dist) < buffer_distance) > 0), TRUE, FALSE)
-    print(Sys.time()-start)
-    print('time to do calculations')
+  add_park_dist <- as.data.frame(sfnetworks::st_network_cost(network_file, from = address_location, to = fake_entrance))
+  closest_park <- apply(add_park_dist, 1, FUN = min)
+  parks_in_buffer <- ifelse((rowSums(units::drop_units(add_park_dist) < buffer_distance) > 0), TRUE, FALSE)
+  print(Sys.time()-start)
+  print('time to do calculations')
 
 
-    if (missing(UID)) {
-      UID <- 1:nrow(address)
-    }
+  if (missing(UID)) {
+    UID <- 1:nrow(address)
+  }
 
-    # if ("UID" %in% colnames(address)) {
-    #   df <- data.frame(address, closest_park, parks_in_buffer)
-    #   df <- sf::st_as_sf(df)
-    # } else{
-    #
-    #   df <- data.frame(UID, address, closest_park, parks_in_buffer)
-    #   df <- sf::st_as_sf(df)
-    # }
-    df <- data.frame(UID, address_location, closest_park, parks_in_buffer)
-    df <- sf::st_as_sf(df)
+  # if ("UID" %in% colnames(address)) {
+  #   df <- data.frame(address, closest_park, parks_in_buffer)
+  #   df <- sf::st_as_sf(df)
+  # } else{
+  #
+  #   df <- data.frame(UID, address, closest_park, parks_in_buffer)
+  #   df <- sf::st_as_sf(df)
+  # }
+  df <- data.frame(UID, address_location, closest_park, parks_in_buffer)
+  df <- sf::st_as_sf(df)
 
-    print(Sys.time()-start_function)
-    print('time to run the entire function')
-
-
-
-    ### Finalizing the output
+  print(Sys.time()-start_function)
+  print('time to run the entire function')
 
 
-    return(df)
+
+  ### Finalizing the output
+
+
+  return(df)
+
+
+
+
+
 }
+
+
 
