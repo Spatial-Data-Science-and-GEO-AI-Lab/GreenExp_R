d635de4a0951b6fd55b7b1bea8fc1710d8f18afc
Martijn Koster <martijn.koster.94@gmail.com>
2023-06-08 13:02:22 +0200

    green_cover + Amsterdam figures

--- R/green_cover.R
+++ R/green_cover.R
@@ -24,9 +24,10 @@
 land_cover <- function(address_location, raster, buffer_distance=NULL, network_buffer=FALSE, download_dir = tempdir(),
                           network_file=NULL, epsg_code=NULL,  UID=NULL, address_calculation = TRUE, speed=NULL, time=NULL,
                           city=NULL, year='2021', plot_landcover=FALSE) {
-  ### Preparation
+  ###### 1. Preperation + Cleaning #######
   start_function <- Sys.time()
-  # Make sure main data set has projected CRS and save it
+
+  # Make sure main data set has projected CRS and save it as default WGS84 if it has geographich coordinates
   if (sf::st_is_longlat(address_location)){
     warning("The CRS in your main data set has geographic coordinates, the Projected CRS will be set to WGS 84 / World Mercator")
     if (missing(epsg_code)) {
@@ -38,12 +39,14 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
       address_location <- sf::st_transform(address_location, projected_crs)
     }
   } else{
-    # If address location is given as projected, save the crs
     projected_crs <- sf::st_crs(address_location)
   }
-  ### Address vs area
+
   if (address_calculation) {
-    ### Check for any polygons, convert into centroids if there are any
+
+
+
+    #Check for any polygons, convert into centroids if there are any
     if ("POINT" %in% sf::st_geometry_type(address_location)) {
     }else if (missing(buffer_distance)) {
       stop("You do not have a point geometry and did not provide a buffer, please provide a point geometry or a buffer distance")
@@ -52,9 +55,11 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
       message('There are nonpoint geometries, they will be converted into centroids')
       address_location <- sf::st_centroid(address_location)
     }
+
+    # If buffer distnace is missing, make sure to calculate it with time and speed,
     if (missing(buffer_distance)){
       if(missing(speed)||missing(time)){
-        stop("You didn't enter speed or time, please enter speed or time, or the buffer distance.")
+        stop("You didn't enter speed or time, please enter speed or time, of the buffer distance.")
       } else if (!speed > 0) {
         stop("Speed must be a positive integer")
       } else if (!time > 0) {
@@ -66,41 +71,34 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
 
     }
 
+    ###### 2. Network buffer ######
     # If people want to calculate the network buffer.
     if (network_buffer) {
       message('You will use a network to create a buffer around the address location(s),
               Keep in mind that for large files it can take a while to run the funciton.')
+      # If the network file is missing create the network file using osmextract
       if(missing(network_file)){
         message('You did not provide a network file, osm will be used to create a network file.')
-
-
-        start <- Sys.time()
-        ### Extracting OSM road structure to build isochrone polygon
+        ###### 2.1 Calculating the network buffer when missing ######
+        # Extracting OSM road structure to build isochrone polygona
         iso_area <- sf::st_buffer(sf::st_convex_hull(
           sf::st_union(sf::st_geometry(address_location))),
           buffer_distance)
         iso_area <- sf::st_transform(iso_area, crs = 4326)
-        # bbox might be redundant
-        bbox <- sf::st_bbox(iso_area)
         # Use the osmextract package to extract the lines in the area.
         if (!missing(city)) {
-          lines <- osmextract::oe_get(city, stringsAsFactors=FALSE, boundary=iso_area, max_file_size = 5e+09,
-                                      download_directory=download_dir, boundary_type = 'spat')
-
+          lines <- osmextract::oe_get(city, stringsAsFactors=FALSE, boundary=iso_area,
+                                      download_directory=download_dir, max_file_size = 5e+09, boundary_type = 'spat')
 
         } else{
           message('If a city is missing, it will take more time to run the function')
-          lines <- osmextract::oe_get(iso_area, stringsAsFactors=FALSE, boundary=iso_area, max_file_size = 5e+09,
-                                      download_directory=download_dir, boundary_type = 'spat')
-
+          lines <- osmextract::oe_get(iso_area, stringsAsFactors=FALSE, boundary=iso_area,
+                                      download_directory=download_dir, max_file_size = 5e+09, boundary_type = 'spat')
         }
 
-        ## We might need the multilinestrings as well?
-        # start<-Sys.time()
-        # b <- osmextract::oe_get(iso_area, stringsAsFactors=FALSE,quiet=T, layer='multilinestrings', boundary=iso_area, boundary_type = 'spat')
-        # print(Sys.time()-start)
-
       }
+
+      # If network file is given, make sure the crs are both the same
       else{
         #Check if the address location and the network file that was given have the same CRS.
         if (sf::st_crs(address_location) != sf::st_crs(network_file))
@@ -108,32 +106,27 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
           print("The CRS of your network data set is geographic, CRS of main data set will be used to transform")
           network_file <- sf::st_transform(network_file, sf::st_crs(address_location))
         }
-
       }
 
-      # now I have the lines I want.
+      ####### 2.2 Calculation of the network files ########
+
+
+      # Give the lines the correct crs, and select items we need
       lines <- sf::st_transform(lines, projected_crs)
       lines <- tidygraph::select(lines, "osm_id", "name")
 
+      # make sure the region is correct crs and trim the lines
       region_shp <- sf::st_transform(iso_area, projected_crs)
-
-      #Download osm used a square bounding box, now trim to the exact boundary
-      #note that lines that that cross the boundary are still included
       lines <- lines[region_shp,]
 
-      # Round coordinates to 0 digits.
-      sf::st_geometry(lines) <- sf::st_geometry(lines) %>%
-        sf::st_sfc(crs = sf::st_crs(lines))
-
-      # Network
+      # convert to a network file
       network_file <- sfnetworks::as_sfnetwork(lines, directed = FALSE)
       network_file <- tidygraph::convert(network_file, sfnetworks::to_spatial_subdivision)
 
-      #convert network to an sf object of edge
-
+      #convert network to an sf object the activated edges
+      start=Sys.time()
       net_sf <- network_file %>% tidygraph::activate("edges") %>%
         sf::st_as_sf()
-
       # Find which edges are touching each other
       touching_list <- sf::st_touches(net_sf)
       # create a graph from the touching list
@@ -153,7 +146,7 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
       network_file <- network_file %>%
         tidygraph::activate("nodes") %>%
         sf::st_filter(osm_connected_edges, .pred = sf::st_intersects)
-
+      start <- Sys.time()
 
       # Compute the edge weights bsased on their length
       network_file <- tidygraph::mutate(tidygraph::activate(network_file, "edges"),
@@ -167,16 +160,13 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
           tidygraph::ungroup()
       }
 
-
-
-
+      # Activate the nodes of the network file
       network_file<- tidygraph::activate(network_file, "nodes")
 
-      iso_list <- list()
-
+      # the amount of iterations
       n_iter <- nrow(address_location)
 
-
+      # Set up a progress bar
       pb <- progress::progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
                                        total = n_iter,
                                        complete = "=",   # Completion bar character
@@ -186,7 +176,8 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
                                        width = 100)      # Width of the progress bar
 
 
-
+      # create an empty list for the nearest features for the address locations
+      iso_list <- list()
       # Calculate nearest features for all address locations
       nearest_features <- sf::st_nearest_feature(address_location, network_file)
       start <- Sys.time()
@@ -195,8 +186,10 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
         tidygraph::filter(network_file, tidygraph::node_distance_from(
           nearest_features[i], weights = weight) <= buffer_distance)
       })
-      n_iter2 <- length(iso_list)
 
+      # iterations
+      n_iter2 <- length(iso_list)
+      # second progress bar
       pb2 <- progress::progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
                                         total = n_iter2,
                                         complete = "=",   # Completion bar character
@@ -236,8 +229,9 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
   else {
     calculation_area <- address_location
   }
-
+###### 3. Raster for land cover #######
   if (missing(raster)){
+###### 3.1 Planetary computer #####
     # make sure the year is a character
     if (!is.character(year)){
       year <- is.character(year)
@@ -374,13 +368,14 @@ land_cover <- function(address_location, raster, buffer_distance=NULL, network_b
       class_raster <- terra::project(class_raster, paste0('EPSG:',epsg_raster))
 
     }
-
-    ### Calculations
     # Extract the landcover values within the buffer
     if (plot_landcover){
       plot <- plot(class_raster)
       print(plot)
     }
+
+#####  4. Calculations #####
+
 
     class_raster_values <- terra::extract(class_raster, calculation_area)
     # count the amount of land cover values per polygon
